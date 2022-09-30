f6d24af4ea06584bc92176134eb58d35a72f701f
Lot Kyqyku <lotkyqyku@gmail.com>
2022-09-30 10:55:10 +0200

    .

--- resources/views/users/register.blade.php
+++ resources/views/users/register.blade.php
@@ -7,7 +7,8 @@
         <p class="mb-4">Create an account to post jobs</p>
     </header>
 
-    <form action="">
+    <form method="POST" action="users">
+        @csrf
         <div class="mb-6">
             <label for="name" class="inline-block text-lg mb-2">
                 Name
@@ -17,6 +18,9 @@
                 class="border border-gray-200 rounded p-2 w-full"
                 name="name"
             />
+            @error('name')
+<p class="text-red-500 text-xs mt-1">{{$message}}</p>
+            @enderror
         </div>
 
         <div class="mb-6">
@@ -28,10 +32,9 @@
                 class="border border-gray-200 rounded p-2 w-full"
                 name="email"
             />
-            <!-- Error Example -->
-            <p class="text-red-500 text-xs mt-1">
-                Please enter a valid email
-            </p>
+            @error('email')
+            <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+            @enderror
         </div>
 
         <div class="mb-6">
@@ -46,6 +49,9 @@
                 class="border border-gray-200 rounded p-2 w-full"
                 name="password"
             />
+            @error('password')
+            <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+             @enderror
         </div>
 
         <div class="mb-6">
@@ -58,8 +64,11 @@
             <input
                 type="password"
                 class="border border-gray-200 rounded p-2 w-full"
-                name="password2"
+                name="password_confirmation"
             />
+            @error('password_confirmation')
+            <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+            @enderror
         </div>
 
         <div class="mb-6">
