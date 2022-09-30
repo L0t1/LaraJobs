08edcd3e20e0dd7f741df12c04abf5b391cf167e
Lot Kyqyku <lotkyqyku@gmail.com>
2022-09-30 10:46:38 +0200

    .

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ListingController;
+use App\Http\Controllers\UserController;
 use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -61,3 +62,6 @@ Route::delete('/listings/{listing}',
 Route::get('/listings/{listing}', 
 [ListingController::class,
 'show']);
+
+//Show Register/Create form
+Route::get('register', [UserController::class,'create']);
